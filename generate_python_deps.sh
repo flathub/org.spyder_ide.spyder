23da5bd9d23320eb6e6495e6af0dcef40f842206
Kavin Teenakul <andythe_great@protonmail.com>
2024-09-09 00:10:49 +0200

    update patch

--- generate_python_deps.sh
+++ generate_python_deps.sh
@@ -4,15 +4,16 @@
 # Mark the script file as executable and run with ./generate_python_deps.sh
 
 
-python3 flatpak-pip-generator setuptools_rust hatchling exceptiongroup pyproject_metadata tomli setuptools_scm_git_archive setuptools_scm meson-python scikit_build_core expandvars maturin -o spyder_deps_additional && # This create some dependencies that is missing. exceptiongroup needed by ipython 8.15.0
+python3 flatpak-pip-generator setuptools_rust hatchling exceptiongroup pyproject_metadata tomli setuptools_scm_git_archive setuptools_scm meson-python scikit_build_core expandvars -o spyder_deps_additional && # This create some dependencies that is missing. exceptiongroup needed by ipython 8.15.0
 # rm -f spyder_*.txt || true && # Remove previous text file if any
 pipgrip spyder > spyder_pipgrip.txt && # pipgrip generate list of dependencies of spyder with pip and write it to a text file, install pipgrip with 'pip3 install pipgrip'
 cp spyder_pipgrip.txt spyder_deps_list.txt && # Create a copy and we will work with the copy, pipgrip take a long time
 sed -i -E '/^(spyder|pyqt|markupsafe|pygments|six)/d' spyder_deps_list.txt && # Remove deps that is already installed
 sed -i -E '/rtree/ s/.*/rtree==1.2.0/' spyder_deps_list.txt &&
 # Move python lib that requires rust to spyder_deps_rust.txt. Rust dependencies is complicated
-grep -E '^(jellyfish|jsonschema|rpds|cryptography|referencing|keyring|secretstorage|nbconvert|nbclient|nbformat|python-lsp-black|black)' spyder_deps_list.txt >> spyder_deps_rust.txt &&
-sed -i -E '/^(jellyfish|jsonschema|rpds|cryptography|referencing|keyring|secretstorage|nbconvert|nbclient|nbformat|python-lsp-black|black)/d' spyder_deps_list.txt &&
+grep -E '^(jellyfish|jsonschema|rpds|cryptography|referencing|keyring|secretstorage|nbconvert|nbclient|nbformat|python-lsp-black|black|asyncssh|pygithub)' spyder_deps_list.txt >> spyder_deps_rust.txt &&
+sed -i -E '/^(jellyfish|jsonschema|rpds|cryptography|referencing|keyring|secretstorage|nbconvert|nbclient|nbformat|python-lsp-black|black|asyncssh|pygithub)/d' spyder_deps_list.txt &&
+# sed -i -e '$amaturin==1.7.1' spyder_deps_rust.txt &&#Manually add python dependency that need rust
 # The spyder_deps_list.txt will generate too large of a json file so split them to spyder_deps_2.txt
 sed -n '1,50p' spyder_deps_list.txt > spyder_deps_1.txt && # Save the first 50 lines to spyder_deps_1.txt
 sed -n '51,100p' spyder_deps_list.txt > spyder_deps_2.txt &&
